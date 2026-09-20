59c30b4f68dedfaf0a9c4930a85b393efc9fb14c
Ultradodoshka <super.duper.dodo00@gmail.com>
2026-09-20 12:45:24 +0500

    Added Windows classes

---
+++ project/src/gui/windows/WindowsCheckBox.java
@@ -0,0 +1,10 @@
+package gui.windows;
+
+import gui.CheckBox;
+
+public class WindowsCheckBox implements CheckBox {
+    @Override
+    public void paint() {
+        System.out.println("WindowsCheckBox paint");
+    }
+}
---
+++ project/src/gui/windows/WindowsFactory.java
@@ -0,0 +1,17 @@
+package gui.windows;
+
+import gui.Button;
+import gui.CheckBox;
+import gui.GUIFactory;
+
+public class WindowsFactory implements GUIFactory {
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+
+    @Override
+    public CheckBox createCheckBox() {
+        return new WindowsCheckBox();
+    }
+}
